25a62db8d5545f3cecf99389f867d40e29916de9
noar-t <noahthornton15@gmail.com>
2018-12-04 21:32:43 -0600

    Changed queue to be shared among all channels

--- channel.py
+++ channel.py
@@ -6,22 +6,23 @@ import multiprocessing as mp
 
 class Channel(object):
 
-    def __init__(self, hosts, port):
+    def __init__(self, hosts, port, out_queue):
         """Initializes a Channel, which is essential a multicast group.
         Also creates a process which will listen on the channel for
         incoming channel messages.
 
         Keyword arguments:
-        hosts -- a list of the host name or ips of the members of the
-                 channel as strings
-        port  -- the port which to send out the udp messages to
+        hosts     -- a list of the host name or ips of the members of the
+                     channel as strings
+        port      -- the port which to send out the udp messages to
+        out_queue -- a multiprocessing queue to place incoming messages
         """
         self.hosts = hosts
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #os.inhertiable?
 
-        self.msg_queue = mp.Queue()
+        self.msg_queue = out_queue
         self.__listener = mp.Process(target=self.__recv_worker, daemon=True)
         self.__listener.start()
 
@@ -30,9 +31,13 @@ class Channel(object):
         for host in self.hosts:
             self.socket.sendto(message, (host, self.port))
 
-    def get_message(self):
-        """ Block until a message is received """
-        return self.msg_queue.get()
+    #def get_message(self):
+    #    """ Block until a message is received """
+    #    return self.msg_queue.get()
+
+    #@property
+    #def queue(self):
+    #    return self.msg_queue
 
     def __recv_worker(self):
         """ Configures the socket for the channel to listen """
